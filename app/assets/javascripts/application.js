ef63eb01a7e0f16f0697a459499c0f149c2ee5c8
Mohamad Adam <adam.mohamad@education.gov.uk>
2019-07-17 14:41:05 +0100

    Heroku App Migration

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,43 @@
+/* global $ */
+/* global GOVUK */
+
+// Warn about using the kit in production
+if (
+  window.sessionStorage && window.sessionStorage.getItem('prototypeWarning') !== 'false' &&
+  window.console && window.console.info
+) {
+  window.console.info('GOV.UK Prototype Kit - do not use for production')
+  window.sessionStorage.setItem('prototypeWarning', true)
+}
+
+$(document).ready(function () {
+  // Show and hide toggled content
+  // Where .multiple-choice uses the data-target attribute
+  // to toggle hidden content
+  var showHideContent = new GOVUK.ShowHideContent()
+  showHideContent.init()
+})
+
+
+// js to support patterns in library
+
+$(document).ready(function() {
+
+    //tabs pattern
+    $(".tabs-menu a").click(function(event) {
+    event.preventDefault();
+    $(this).parent().addClass("current");
+    $(this).parent().siblings().removeClass("current");
+    var tab = $(this).attr("href");
+    $(".tab-content").not(tab).css("display", "none");
+    $(tab).fadeIn();
+    });
+    
+    
+    $(".toggler").click(function(e){
+        e.preventDefault();
+        $(this).parent().toggleClass("closed")
+        $(this).next().toggleClass("closed")
+    })
+
+});  
